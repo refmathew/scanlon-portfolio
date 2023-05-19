26d6400c5818497837af4a0102cda20e86d71315
Mathew Reforma <reformamathew@protonmail.com>
2023-05-19 17:23:57 +0800

    style: format nuxt config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,25 +2,6 @@ import { dir } from "console";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    // components: [ 
-        // {
-            // pathPrefix: false,
-        // } 
-    // ]
-    // vite: {
-    //     css: {
-    //         preprocessorOptions: {
-    //             scss: {
-    //                 // prependData: '@use "@/assets/styles/abstracts" as abs'               
-    //             }
-    //         }
-    //     }
-    // },
-    css: [
-        '@/assets/styles/base/_index.scss'
-    ]
-    ,
-    dir: {
-        static: "static"
-    }
+    css: [ '@/assets/styles/base/_index.scss' ],
+    dir: { static: "static" }
 })
