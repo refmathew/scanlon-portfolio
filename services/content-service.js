a0560d293c5be7cd691c9fb2c40473fa923b0b6e
Mathew Reforma <reformamathew@protonmail.com>
2023-06-20 14:48:40 +0800

    Add function to fetch "about content" in Contentservice

--- services/content-service.js
+++ services/content-service.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
 class ContentService {
-  static async FetchHomeContent() {
-    const response = await axios.get("/data/pages/home.json");
-    return response.data;
-  }
+	static async FetchHomeContent() {
+		const response = await axios.get("/data/pages/home.json");
+		return response.data;
+	}
+	static async FetchAboutContent() {
+		const response = await axios.get("/data/pages/about.json");
+		return response.data;
+	}
 }
 
 export default ContentService;
