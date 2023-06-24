cbf39195e3088c866fb0a09a0fe44cb784ce3852
Mathew Reforma <reformamathew@protonmail.com>
2023-06-25 03:20:10 +0800

    fix: prevent element from suddenly disappearing when reference element disappears

--- composables/useRevealAnimation.js
+++ composables/useRevealAnimation.js
@@ -4,11 +4,14 @@ export default (
 	$reference, // reveal relative to this reference element
 	config
 ) => {
-	// Vertical offset is for the elements who are animationg
+	// Vertical offset is for the elements which are animating from bottom
+	// where position is higher than before animation class is applied
 	const { revealPoint = 128, verticalOffset = 64 } = config || {};
 
 	window.addEventListener("scroll", () => {
+		const originalElTopOffset = $el.getBoundingClientRect().top;
 		let elTopOffset;
+
 		// if there's a reference element, use it instead
 		$reference
 			? (elTopOffset = $reference.getBoundingClientRect().top)
@@ -18,7 +21,12 @@ export default (
 			return $el.classList.add(classToAdd);
 		}
 
-		if (elTopOffset > window.innerHeight) {
+		// OriginalElTopOffset is used instead of the eltopoffset
+		// (either the element itself or the reference element)
+		// so that when there's a reference element used which is lower than
+		// the element itself, the element would not disappear together with
+		// the reference element
+		if (originalElTopOffset > window.innerHeight) {
 			return $el.classList.remove(classToAdd);
 		}
 	});
